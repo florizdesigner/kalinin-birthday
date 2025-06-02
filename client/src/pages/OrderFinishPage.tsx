c765ec033d4bc526c6f0213acb0f014860920b29
florizdesigner <vyacheslav.sokolovv@icloud.com>
2025-06-02 09:06:58 +0300

    add order-finish-page

--- client/src/pages/OrderFinishPage.tsx
+++ client/src/pages/OrderFinishPage.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useRef, useState} from 'react';
 import FilaLogo from '../assets/fila.png';
 import { motion } from 'framer-motion';
+import SuccessAudio from '../assets/success.mp3';
 
 const OrderFinishPage = () => {
-    const [showImage, setShowImage] = useState(true);
+    const [showImage, setShowImage] = useState(false);
+    const audioRef = useRef<HTMLAudioElement | null>(null);
 
     useEffect(() => {
         // Показываем изображение через 1 секунду
@@ -11,9 +13,21 @@ const OrderFinishPage = () => {
             setShowImage(true);
         }, 1000);
 
-        return () => clearTimeout(showTimer);
-    }, []);
+        // Создаем элемент audio и воспроизводим музыку
+        audioRef.current = new Audio(SuccessAudio);
+        audioRef.current.loop = true; // Зацикливаем музыку
+        audioRef.current.volume = 0.5; // Устанавливаем громкость (0-1)
+        audioRef.current.play().catch(e => console.log("Автовоспроизведение заблокировано:", e));
 
+        return () => {
+            clearTimeout(showTimer);
+            // Останавливаем музыку при размонтировании компонента
+            if (audioRef.current) {
+                audioRef.current.pause();
+                audioRef.current = null;
+            }
+        };
+    }, []);
 
     return (
         <div className="relative min-h-screen bg-gradient-to-tr from-white flex-col via-red-50 to-red-100 flex items-center justify-center font-fancy p-4">
